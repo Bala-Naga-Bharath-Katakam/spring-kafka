4047fcead37d65e47b317b61ae08ac2b919b48ea
Bala-Naga-Bharath-Katakam <bharathkatak173@gmail.com>
2023-07-31 13:54:34 +0530

    adding advanced streams

--- advanced-streams/src/main/java/com/learnkafkastreams/topology/ExploreWindowTopology.java
+++ advanced-streams/src/main/java/com/learnkafkastreams/topology/ExploreWindowTopology.java
@@ -8,15 +8,64 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.*;
 
 import java.time.*;
-import java.time.format.DateTimeFormatter;
 
 @Slf4j
 public class ExploreWindowTopology {
 
     public static final String WINDOW_WORDS = "windows-words";
 
-    public static Topology build(){
+    public static Topology tumblingWindows(){
         StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        KStream<String, String> kStream = streamsBuilder.stream(WINDOW_WORDS, Consumed.with(Serdes.String(), Serdes.String()));
+
+        Duration windowSize=Duration.ofSeconds(5);
+
+        TimeWindows timeWindow=TimeWindows.ofSizeWithNoGrace(windowSize);
+
+        TimeWindowedKStream<String, String> timeWindowedKStream = kStream.groupByKey().windowedBy(timeWindow);
+
+        KTable<Windowed<String>, Long> count = timeWindowedKStream.count();
+
+        count.toStream().peek((k,v)->log.info("key and values are : {} {} ",k,v ));
+
+        return streamsBuilder.build();
+    }
+
+    public static Topology hoppingWindow(){
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        KStream<String, String> kStream = streamsBuilder.stream(WINDOW_WORDS, Consumed.with(Serdes.String(), Serdes.String()));
+
+        Duration windowSize=Duration.ofSeconds(5);
+
+        Duration advancedSize=Duration.ofSeconds(5);
+
+        TimeWindows timeWindow=TimeWindows.ofSizeWithNoGrace(windowSize).advanceBy(advancedSize);
+
+        TimeWindowedKStream<String, String> timeWindowedKStream = kStream.groupByKey().windowedBy(timeWindow);
+
+        KTable<Windowed<String>, Long> count = timeWindowedKStream.count();
+
+        count.toStream().peek((k,v)->log.info("key and values are : {} {} ",k,v ));
+
+        return streamsBuilder.build();
+    }
+
+    public static Topology slidingWindow(){
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        KStream<String, String> kStream = streamsBuilder.stream(WINDOW_WORDS, Consumed.with(Serdes.String(), Serdes.String()));
+
+        Duration windowSize=Duration.ofSeconds(5);
+
+        SlidingWindows timeWindow=SlidingWindows.ofTimeDifferenceWithNoGrace(windowSize);
+
+        TimeWindowedKStream<String, String> timeWindowedKStream = kStream.groupByKey().windowedBy(timeWindow);
+
+        KTable<Windowed<String>, Long> count = timeWindowedKStream.count();
+
+        count.toStream().peek((k,v)->log.info("key and values are : {} {} ",k,v ));
 
         return streamsBuilder.build();
     }
